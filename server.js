d9331fae133e0a90aac0e9eff202bd2fe62e60e7
leenahmad <leenkaraja24@gmail.com>
2021-09-28 23:29:27 +0300

    done lab13

--- server.js
+++ server.js
@@ -10,17 +10,27 @@ const server = express();
 
 server.use(cors());
 
-const PORT = process.env.PORT;
 
-const getBooksHandler = require("./modules/Book");
+const PORT = process.env.PORT;
+server.use(express.json());
+
+const getBooksHandler = require('./modules/Book');
+
+const addBook = require('./modules/Book');
+
+const deleteHandler = require('./modules/Book')
+
 const mongoose = require('mongoose') 
 
 mongoose.connect('mongodb://localhost:27017/books');
 
 // const axios = require('axios');
 
+
 server.get('/' , home);
 server.get('/books',getBooksHandler)
+server.post('/addBook', addBook)
+server.delete('/deleteBook', deleteHandler)
 server.get("*", notFound);
 
 
